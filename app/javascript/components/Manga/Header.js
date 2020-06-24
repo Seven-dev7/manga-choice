5903a7362174ba7f823e0e0e44c53217d3d57b0f
Se7en Oussama <seven.tdr7@gmail.com>
2020-06-24 17:46:51 +0100

    import styled components and create all const in Header and Manga

--- app/javascript/components/Manga/Header.js
+++ app/javascript/components/Manga/Header.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import styled from 'styled-components'
+
+
+const Wrapper = styled.div`
+    padding: 50px 100px 50px 0;
+    font-size: 30px;
+
+    img {
+        height: 200px;
+        width: 200px;
+        border-radius: 80%;
+        border: 1px solid rgba(0,0,0,0.1);
+        margin-bottom: -8px;
+    }
+`
+const TotalReviews = styled.div`
+    font-size: 18px;
+    padding: 10px 0;
+`
+const TotalOutof = styled.div`
+    font-size: 18px;
+    font-weight: bold;
+    padding: 10px 0;
+`
+
 
 const Header = (props) => {
     const {name, image_url, avg_score} = props.attributes
     const total = props.reviews.length
     return(
-        <div className="wrapper">
+        <Wrapper>
             <h1>
                 <img src={image_url} alt={name} />
                 {name}
             </h1>
-            <div className="totalReviews">
+            <TotalReviews>
                 {total} User Reviews
-            </div>
+            </TotalReviews>
             <div className="starRating">
 
             </div>
-            <div className="totalOutof">
+            <TotalOutof>
                 {avg_score}
-            </div>
-        </div>
+            </TotalOutof>
+        </Wrapper>
     )
 }
 
--- app/javascript/components/Manga/Manga.js
+++ app/javascript/components/Manga/Manga.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr)
+`
+const Column = styled.div`
+    background: #fff;
+    height: 100vh;
+
+    &:last-child {
+        background: #000;
+    }
+`
+const Main = styled.div`
+    padding-left: 50px;
+`
 
 const Manga = (props) => {
     const [manga, setManga] = useState({})
@@ -22,26 +41,28 @@ const Manga = (props) => {
     }, [])
 
     return(
-        <div className="wrapper">
-            <div className="column">
+        <Wrapper>
+            <Column>
+                <Main>
                 {
                     loaded &&
                     <Header
                         attributes={manga.data.attributes}
                         reviews={manga.included}
                     />
-                }               
+                }
                 <div className="reviews">
-
+                    
                 </div>
-            </div>
-            <div className="column">
+                </Main>
+            </Column>
+            <Column>
                 <div className="review-form">
                     Review comes here
                 </div>
-            </div>
+            </Column>
             This is Manga#Show view
-        </div>
+        </Wrapper>
     )
 }
 

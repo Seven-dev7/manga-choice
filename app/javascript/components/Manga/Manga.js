8e826799d63a676ded8d049e7afe22ec34a6f021
Se7en Oussama <seven.tdr7@gmail.com>
2020-06-24 17:26:11 +0100

    add data in show Manga

--- app/javascript/components/Manga/Manga.js
+++ app/javascript/components/Manga/Manga.js
@@ -1,8 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './Header'
 
-const Manga = () => {
+const Manga = (props) => {
+    const [manga, setManga] = useState({})
+    const [review, setRanga] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        const slug = props.match.params.slug
+        const url = `/api/v1/mangas/${slug}`
+
+        axios.get(url)
+        .then( resp => {
+            setManga(resp.data)
+            setLoaded(true)    
+        })
+        .catch( resp => console.log(resp) )
+        // api/v1/mangas/kingdom
+        // mangas/kingdom
+    }, [])
+
     return(
-        <div>
+        <div className="wrapper">
+            <div className="column">
+                {
+                    loaded &&
+                    <Header
+                        attributes={manga.data.attributes}
+                        reviews={manga.included}
+                    />
+                }               
+                <div className="reviews">
+
+                </div>
+            </div>
+            <div className="column">
+                <div className="review-form">
+                    Review comes here
+                </div>
+            </div>
             This is Manga#Show view
         </div>
     )

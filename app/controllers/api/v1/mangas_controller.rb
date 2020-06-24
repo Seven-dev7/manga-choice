a1de7526ed11da2c7e5edc7f169fbd21d4f6a609
Se7en Oussama <seven.tdr7@gmail.com>
2020-06-24 14:09:06 +0100

    add reviews controller

--- app/controllers/api/v1/mangas_controller.rb
+++ app/controllers/api/v1/mangas_controller.rb
@@ -3,13 +3,13 @@ module Api
         class MangasController < ApplicationController
             def index
                 mangas = Manga.all
-                render json: MangaSerializer.new(mangas).serialized_json
+                render json: MangaSerializer.new(mangas, options).serialized_json
             end
 
             def show
                 manga = Manga.find_by(slug: params[:slug])
 
-                render json: MangaSerializer.new(manga).serialized_json                
+                render json: MangaSerializer.new(manga, options).serialized_json                
             end
 
             def create
@@ -26,7 +26,7 @@ module Api
                 manga = Manga.find_by(slug: params[:slug])
 
                 if manga.update(manga_params)
-                    render json: MangaSerializer.new(manga).serialized_json
+                    render json: MangaSerializer.new(manga, options).serialized_json
                 else
                     render json: { error: manga.errors.messages }, status: 422
                 end
@@ -47,6 +47,10 @@ module Api
 
             def manga_params
                 params.require(:manga).permit(:name, :image_url)
+            end
+
+            def options
+                @options ||= { include: %i[reviews] }
             end
         end
     end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,31 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
-            
+            def create
+                review = Review.create(review_params)
+
+                if review.save
+                    render json: ReviewSerializer.new(review).serialized_json
+                else
+                    render json: { error: review.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                review = Review.find(params[:id])
+
+                if review.destroy
+                    head :no_content
+                else
+                    render json: { error: review.errors.messages }, status: 422
+                end
+            end
+
+            private
+
+            def review_params
+                params.require(:reviews).permit(:title, :description, :score, :manga_id)
+            end
         end
     end
 end
